3a06b347017065b5dc0914841afa28a462a114e5
sketchi <144040587+DalaSketchi@users.noreply.github.com>
2023-09-13 19:01:07 +0800

    Code
    Main Code

---
+++ m4sCompose/m4sCompose.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+#include<Windows.h>
+using namespace std;
+//Verson 1.1
+string GetName(string a, string b)
+{
+	string use = a.size() <= b.size() ? a : b;
+	if (use.find(".m4s\"") != string::npos)return use.erase(use.find(".m4s\""), 5) + ".mp4\"";
+	return use + ".mp4";
+}
+int main()
+{
+	string a, b;
+	cout << "声音：";
+	cin >> a;
+	cout << "视频：";
+	cin >> b;
+	string wz = "ffmpeg -i " + a + " -i " + b + " " + GetName(a, b);
+	system(wz.c_str());
+	return 0;
+}
